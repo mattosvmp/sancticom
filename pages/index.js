651c4880c43c763663e58f7892bbcee9c753c7d1
Guilherme Mattos <mattosvmp@gmail.com>
2024-09-07 01:55:03 +0000

    init: rework do projeto rosarium

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+  return <div>
+    <h1>Bem-vinda</h1>
+    <p>Olá minha caríssima, eu gostaria de lhe dar boas vindas nesse projeto!</p>
+    <p>É algo bem singelo, mas é um pequeno terreno que conquisto, e apartir disto conquistar outros. É pouco o serviço que presto, sendo como sou, um miserável pecador.</p>
+  </div>
+}
+
+export default Home;
