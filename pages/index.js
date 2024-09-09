c00d443b009ee5be395a8f83ac6a50977a9097d4
Guilherme Mattos <mattosvmp@gmail.com>
2024-09-09 13:41:20 +0000

    feat: adiciona o arquivo .gitignore

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ function Home() {
   return <div>
     <h1>Bem-vinda</h1>
     <p>Olá minha caríssima, eu gostaria de lhe dar boas vindas nesse projeto!</p>
-    <p>É algo bem singelo, mas é um pequeno terreno que conquisto, e apartir disto conquistar outros. É pouco o serviço que presto, sendo como sou, um miserável pecador.</p>
+    <p>É algo bem singelo, mas é um pequeno terreno que conquisto, e apartir disto conquistar outros. É pouco o serviço que presto, sendo como sou, um verme, miserável e pecador. E nada mais justo do que retribuir a miserícordia de Deus e glorifica-Lo, e simplesmente meditar, com um maior repertório, toda a Sua vida.</p>
+    <p>Se você gostou dessa ideia, me dê metade do seu Milka Strawberry! kkkkkkk</p>
   </div>
 }
 
