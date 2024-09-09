3132adf6341ca4c474394ca3ab7cdf6d490bcf63
Guilherme Mattos <mattosvmp@gmail.com>
2024-09-09 22:16:10 +0000

    adiciona scripts para `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
 function Home() {
-  return <div>
-    <h1>Bem-vinda</h1>
-    <p>Olá minha caríssima, eu gostaria de lhe dar boas vindas nesse projeto!</p>
-    <p>É algo bem singelo, mas é um pequeno terreno que conquisto, e apartir disto conquistar outros.<br/> É pouco o serviço que presto, sendo como sou, um verme, miserável e pecador...<br/> E nada mais justo do que retribuir a miserícordia de Deus e glorifica-Lo, e simplesmente meditar, com um maior repertório, toda a Sua vida.</p>
-    <p>Se você gostou dessa ideia, me dê metade do seu Milka Strawberry 👀 kkkkkkk</p>
-  </div>
+  return (
+    <div>
+      <h1>Bem-vinda</h1>
+      <p>
+        Olá minha caríssima, eu gostaria de lhe dar boas vindas nesse projeto!
+      </p>
+      <p>
+        É algo bem singelo, mas é um pequeno terreno que conquisto, e apartir
+        disto conquistar outros.
+        <br /> É pouco o serviço que presto, sendo como sou, um verme, miserável
+        e pecador...
+        <br /> E nada mais justo do que retribuir a miserícordia de Deus e
+        glorifica-Lo, e simplesmente meditar, com um maior repertório, toda a
+        Sua vida.
+      </p>
+      <p>
+        Se você gostou dessa ideia, me dê metade do seu Milka Strawberry 👀
+        kkkkkkk
+      </p>
+    </div>
+  );
 }
 
 export default Home;
