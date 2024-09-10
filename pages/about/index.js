06295b23c191185c3c11433190545da1c6255f2d
Guilherme Mattos <mattosvmp@gmail.com>
2024-09-10 18:47:49 +0000

    feat: adicionando o About do projeto

---
+++ pages/about/index.js
@@ -0,0 +1,20 @@
+function About() {
+  return (
+    <div>
+      <h1>Sobre</h1>
+      <h2>Propósito:</h2>
+      <p>
+        Criar uma comunidade de melhoria de vida através do Santo Rosário, nisso
+        envolver as pessoas em um ambiente também de direção espiritual, nisso
+        ser repetido pelos comentários dos próprios usuários.
+      </p>
+      <h2>Perfil do usuário:</h2>
+      <p>
+        Alguém aberto a se desenvolver a partir do seu próprio desenvolvimento
+        espiritual, e através disso revelar com sabedoria, seu testemunho.
+      </p>
+    </div>
+  );
+}
+
+export default About;
