71340f297cecd9d960fe50b2d8b1c02ecab958cf
Guilherme Mattos <mattosvmp@gmail.com>
2024-09-21 14:25:13 -0400

    test: rename all descriptions to use new pattern

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -35,7 +35,7 @@ export default async function migrations(request, response) {
       });
 
       if (migratedMigrations.length > 0) {
-        return response.status(200).json(migratedMigrations);
+        return response.status(201).json(migratedMigrations);
       }
 
       return response.status(200).json(migratedMigrations);
