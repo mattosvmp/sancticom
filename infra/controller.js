514a09eaf68fce7fe985a947aa601d83fdcc1b81
Guilherme Mattos <mattosvmp@gmail.com>
2025-06-06 16:48:56 -0400

    feat: add user creation functionality with validation and error handling

--- infra/controller.js
+++ infra/controller.js
@@ -1,4 +1,8 @@
-import { InternalServerError, MethodNotAllowedError } from "infra/errors.js";
+import {
+  InternalServerError,
+  MethodNotAllowedError,
+  ValidationError,
+} from "infra/errors.js";
 
 function onNoMatchHandler(request, response) {
   const publicErrorObject = new MethodNotAllowedError();
@@ -6,12 +10,16 @@ function onNoMatchHandler(request, response) {
 }
 
 function onErrorHandler(error, request, response) {
+  if (error instanceof ValidationError) {
+    return response.status(error.statusCode).json(error);
+  }
+
   const publicErrorObject = new InternalServerError({
     statusCode: error.statusCode,
     cause: error,
   });
 
-  console.log(publicErrorObject);
+  console.error(publicErrorObject);
 
   response
     .status(500)
